ed984a6e92fde5e856eaf85ad6960bbf84788e15
kcalva <kcalva737@gmail.com>
2024-07-24 16:47:44 -0500

    adding associations and validations for each model table i created

--- app/models/property.rb
+++ app/models/property.rb
@@ -10,4 +10,13 @@
 #  landlord_id :integer
 #
 class Property < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :landlord_id, presence: true
+
+  belongs_to :landlord, class_name: "User"
+  has_many :units, dependent: :destroy
+
+  has_many :tenants, through: :units, source: :tenant
+  has_many :payments, through: :units, source: :payments
 end
