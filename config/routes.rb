de02c4b60d1744e1f856568cb696670a56ae8106
kcalva <kcalva737@gmail.com>
2024-08-07 15:32:51 -0500

    refactored code to get rid of additional routes that I did not need. will still check later on for routes that I will not need.

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root to: "landing_page#home"
 
-  resources :messages
-  resources :leases
-  resources :payments do
-    collection do
-      get "tenant/:tenant_id", to: "payments#index", as: "tenant"
-    end
-  end
-  resources :units
   resources :properties do
     get "units", on: :member, to: "properties#units"
   end
 
-  get "dashboard", to: "dashboard#index", as: "dashboard_index"
-  get "tenant/:id", to: "dashboard#tenant", as: "tenant"
-
-  resources :tenants, only: [] do
+  resources :tenant, only: [:show] do
     resources :leases, only: [:index]
+    resources :payments, only: [:index]
   end
+
+  resources :messages
+  resources :leases
+  resources :units
 end
