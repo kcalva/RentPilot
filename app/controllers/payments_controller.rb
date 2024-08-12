21a7b27862b9b1cfd064f97504d8d32c94f896ee
kcalva <kcalva737@gmail.com>
2024-08-12 15:36:18 -0500

    refactoring payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,12 +7,12 @@ class PaymentsController < ApplicationController
     if params[:user_id]
       @tenant = User.find(params[:user_id])
       @payments = @tenant.payments
-      @overdue_payments = @payments.where(status: "overdue").order(due_date: :desc)
-      @pending_payments = @payments.where(status: "pending").order(due_date: :desc)
-      @paid_payments = @payments.where(status: "paid").order(due_date: :desc)
     else
-      @payments = Payment.all
+      @payments = Payment.all.includes(:tenant, :unit)
     end
+    @overdue_payments = @payments.where(status: "overdue").order(due_date: :desc)
+    @pending_payments = @payments.where(status: "pending").order(due_date: :desc)
+    @paid_payments = @payments.where(status: "paid").order(due_date: :desc)
   end
 
   # GET /payments/new
@@ -22,7 +22,6 @@ class PaymentsController < ApplicationController
 
   # GET /payments/1/edit
   def edit
-    @payment = Payment.find(params[:id])
     @tenant = @payment.tenant
   end
 
@@ -31,32 +30,25 @@ class PaymentsController < ApplicationController
     @payment = @tenant.payments.new(payment_params)
     @payment.unit = @unit
 
-    respond_to do |format|
-      if @payment.save
-        format.html { redirect_to user_payments_path(@payment.tenant), notice: "Payment was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @payment.save
+      redirect_to user_payments_path(@payment.tenant), notice: "Payment was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /payments/1
   def update
-    respond_to do |format|
-      if @payment.update(payment_params)
-        format.html { redirect_to user_payments_path(@payment.tenant), notice: "Payment was successfully updated." }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @payment.update(payment_params)
+      redirect_to user_payments_path(@payment.tenant), notice: "Payment was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /payments/1
   def destroy
     @payment.destroy!
-    respond_to do |format|
-      format.html { redirect_to user_payments_path(@payment.tenant), notice: "Payment was successfully destroyed." }
-    end
+    redirect_to user_payments_path(@payment.tenant), notice: "Payment was successfully destroyed."
   end
 
   private
