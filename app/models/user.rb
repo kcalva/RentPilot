868a1298bf89af280a372507d2be4b0cda32f889
kcalva <kcalva737@gmail.com>
2024-08-13 12:15:23 -0500

    adding rails-admin gem to update roles of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
+#  admin                  :boolean          default(FALSE), not null
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  name                   :string
@@ -44,6 +45,10 @@ class User < ApplicationRecord
 
   def tenant?
     role == "tenant"
+  end
+
+  def admin?
+    admin
   end
 
   private
